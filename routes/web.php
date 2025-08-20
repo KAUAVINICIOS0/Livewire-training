27bc397e7cbb8b06b7dd665630836e549cef68be
KAUAVINICIOS0 <kaua.vinicios_2005@hotmail.com>
2025-08-20 13:30:23 -0300

    fix: edit struture of project

--- routes/web.php
+++ routes/web.php
@@ -3,11 +3,11 @@
 use Livewire\Volt\Volt;
 use Illuminate\Support\Facades\Route;
 
-Volt::route('/form-read-color', 'read-color');
+Volt::route('/form-read-color', 'color.read-color');
 
-Volt::route('/form-delete-color/{colorid}', 'read-color');
+Volt::route('/form-delete-color/{colorid}', 'color.read-color');
 
-Volt::route('/form-create-color', 'create-color');
+Volt::route('/form-create-color', 'color.create-color');
 
-Volt::route('/form-update-color/{colorid}', 'update-color');
+Volt::route('/form-update-color/{colorid}', 'color.update-color');
 
